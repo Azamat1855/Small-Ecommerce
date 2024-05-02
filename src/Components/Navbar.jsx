e024e9f62bcd0fb7b66e090f90c351c921f98d14
Azamat1855 <azamatshomaksudov7@gmail.com>
2024-05-02 22:07:08 +0500

    ASD

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="bg-[#17140c] flex items-center py-[30px]">
+    <nav className="bg-[#17140c] flex items-center py-[10px]">
       <div className="w-[85%] mx-auto flex items-center justify-between">
-      <div className="text-white bg-[#47a51f] w-[70px] h-[70px] rounded-full flex justify-center items-center absolute">LOGO</div>
+      <div className="text-white bg-[#47a51f] w-[70px] h-[70px] rounded-full flex justify-center items-center">LOGO</div>
       <div className="flex w-[80%] text-white justify-center items-center mx-auto gap-[10%]">
       <Link to={"/"}><p className="hover:text-gray-300 active:scale-90 transition duration-300">Home</p></Link>
       <Link to={"/products"}><p className="hover:text-gray-300 active:scale-90 transition duration-300">Products</p></Link>
